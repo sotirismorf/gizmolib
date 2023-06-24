2b70869f1d12da7b75fb58457f286e30b09e7897
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2023-06-24 16:43:30 +0300

    add env vars for db connections

--- cmd/microservice/config/config.go
+++ cmd/microservice/config/config.go
@@ -3,7 +3,7 @@ package config
 import (
 	"bytes"
 	_ "embed"
-	"strings"
+	// "strings"
 
 	"github.com/spf13/viper"
 )
@@ -22,13 +22,24 @@ type Config struct {
 }
 
 func Read() (*Config, error) {
-	// Environment variables
-	viper.AutomaticEnv()
-	viper.SetEnvPrefix("APP")
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
-
-	// Configuration file
+	viper.AllowEmptyEnv(false)
 	viper.SetConfigType("yml")
+
+	// Defaults
+	viper.SetDefault("app.admin.password", "password")
+	viper.SetDefault("app.admin.username", "admin")
+	viper.SetDefault("postgres.database", "postgres")
+	viper.SetDefault("postgres.host", "localhost")
+	viper.SetDefault("postgres.password", "password")
+	viper.SetDefault("postgres.user", "postgres")
+
+	// Environment variables
+	viper.BindEnv("app.admin.password", "GL_ADMIN_PASSWORD")
+	viper.BindEnv("app.admin.username", "GL_ADMIN_USER")
+	viper.BindEnv("postgres.database", "PG_DATABASE")
+	viper.BindEnv("postgres.host", "PG_HOST")
+	viper.BindEnv("postgres.password", "PG_PASSWORD")
+	viper.BindEnv("postgres.user", "PG_USER")
 
 	// Read configuration
 	if err := viper.ReadConfig(bytes.NewBuffer(defaultConfiguration)); err != nil {
@@ -40,5 +51,6 @@ func Read() (*Config, error) {
 	if err := viper.Unmarshal(&config); err != nil {
 		return nil, err
 	}
+
 	return &config, nil
 }
