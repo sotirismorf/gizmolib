01131c6935cde80e8bfaed715a7f48a8f56f6f75
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2023-06-26 00:28:28 +0300

    camelCase api

--- models/book.go
+++ models/book.go
@@ -6,22 +6,22 @@ type ApiBookPartialUpdate struct {
 }
 
 type ApiBook struct {
-	ID              int64
+	ID              int64  `json:"id"`
 	Title           string `json:"title,omitempty" binding:"required,max=32"`
 	Description     string `json:"description,omitempty" binding:"required"`
-	AuthorID        int64
-	YearPublished   int16
-	CopiesAvailable int32
-	CopiesTotal     int32
+	AuthorID        int64  `json:"authorId"`
+	YearPublished   int16  `json:"yearPublished"`
+	CopiesAvailable int32  `json:"copiesAvailable"`
+	CopiesTotal     int32  `json:"copiesTotal"`
 }
 
 type ApiBookFull struct {
-	ID              int64
-	AuthorID        int64
+	ID              int64  `json:"id"`
+	AuthorID        int64  `json:"authorId"`
 	Title           string `json:"title,omitempty" binding:"required,max=32"`
 	AuthorName      string `json:"name,omitempty" binding:"required"`
 	Description     string `json:"description,omitempty" binding:"required"`
-	YearPublished   int16
-	CopiesAvailable int32
-	CopiesTotal     int32
+	YearPublished   int16  `json:"yearPublished"`
+	CopiesAvailable int32  `json:"copiesAvailable"`
+	CopiesTotal     int32  `json:"copiesTotal"`
 }
